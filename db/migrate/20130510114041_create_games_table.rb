0d6fa8243cb4a000efb501e2af633b52fe964ab4
stephenitis <nguyenhstephen@gmail.com>
2013-05-10 11:42:45 -0700

    houston we have migration

---
+++ db/migrate/20130510114041_create_games_table.rb
@@ -0,0 +1,11 @@
+class CreateGamesTable < ActiveRecord::Migration
+  def change
+    create_table :games do |t|
+      t.string :board, :default => "---------"
+      t.integer :player1_id
+      t.integer :player2_id
+      t.integer :winner_id
+      t.timestamps
+    end
+  end
+end
